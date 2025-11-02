9e54deaef565bb21ccc2e89ba08be7001b20aa9e
DilshadNaleem <dilshadnaleem13@gmail.com>
2025-11-03 00:26:15 +0530

    adding dist

--- Personal-Blog/vite.config.js
+++ Personal-Blog/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-   server: {
+  server: {
     fs: {
-      allow: ['..', 'node_modules/devicon'] // Allow devicon fonts
+      allow: ['..', 'node_modules/devicon']
     }
   },
-  assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2']
+  assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2'],
+  build: {
+    outDir: 'dist', // Explicitly set output directory
+    assetsDir: 'assets' // Organize assets in subfolder
+  }
 })
